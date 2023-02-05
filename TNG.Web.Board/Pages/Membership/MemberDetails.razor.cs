294effb4505b8cd001c027eb65c90ba86078eeaf
Drake Perrior-Small <drakecsmall@gmail.com>
2023-02-05 00:41:40 -0700

    Add member edit/add page

---
+++ TNG.Web.Board/Pages/Membership/MemberDetails.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using TNG.Web.Board.Data;
+using TNG.Web.Board.Data.DTOs;
+
+namespace TNG.Web.Board.Pages.Membership
+{
+    public partial class MemberDetails
+    {
+        [Parameter]
+        public Guid? memberId { get; set; }
+
+
+#nullable disable
+        [Inject]
+        private ApplicationDbContext context { get; set; }
+
+        [Inject]
+        private NavigationManager navigation { get; set; }
+#nullable enable
+
+        private Member GetMember()
+            => context.Members.FirstOrDefault(m => m.Id == memberId) ?? new();
+
+        private Member? _member { get; set; }
+        private Member Member
+        {
+            get
+            { return _member ??= GetMember(); }
+        }
+
+        private async void UpdateMember()
+        {
+            if (memberId.HasValue)
+                context.Update(Member);
+            else
+                context.Add(Member);
+            await context.SaveChangesAsync();
+            memberId = Member.Id;
+            navigation.NavigateTo("/members/");
+        }
+
+        private void MembershipTypeChange(ChangeEventArgs e)
+        {
+            var membershipType = (MemberType)int.Parse(e.Value!.ToString()!);
+            Member.MemberType = membershipType;
+        }
+    }
+}
