1d2e2fe609f6a92cc95ef2f2b575070868f7e7d6
Drake Perrior-Small <drakecsmall@gmail.com>
2023-02-07 23:03:52 -0700

    Make membership traits removable, update form as notes/suspensions added

--- TNG.Web.Board/Pages/Membership/MemberDetails.razor.cs
+++ TNG.Web.Board/Pages/Membership/MemberDetails.razor.cs
@@ -34,6 +34,14 @@ namespace TNG.Web.Board.Pages.Membership
             { return _member ??= GetMember(); }
         }
 
+        private MembershipPayment? Dues => Member?.Payments?.OrderByDescending(p => p.PaidOn).FirstOrDefault();
+        private MembershipOrientation? Orientation => Member?.Orientations?.OrderByDescending(o => o.DateReceived).FirstOrDefault();
+
+        private DateTime? PaidOnDate => Dues?.PaidOn;
+        private DateTime? OrientationOnDate => Orientation?.DateReceived;
+        private MembershipSuspension? ActiveSuspension => Member?.Suspensions?.OrderByDescending(s => s.EndDate ?? DateTime.MaxValue).FirstOrDefault(s => !s.EndDate.HasValue || s.EndDate >= DateTime.Now);
+
+
         private bool AddDuesPaidToggle { get; set; } = false;
         private DateTime? NewDuesPaid { get; set; }
 
@@ -51,34 +59,27 @@ namespace TNG.Web.Board.Pages.Membership
 
         private async void UpdateMember()
         {
-            if (memberId.HasValue)
-                context.Update(Member);
-            else
-                context.Add(Member);
-            await context.SaveChangesAsync();
+            try
+            {
+                if (memberId.HasValue)
+                    context.Update(Member);
+                else
+                    context.Add(Member);
 
-            if (NewDuesPaid.HasValue)
-            {
-                context.Add(new MembershipPayment { MemberId = Member.Id, PaidOn = NewDuesPaid.Value });
-                await context.SaveChangesAsync();
+                if (NewDuesPaid.HasValue)
+                {
+                    context.Add(new MembershipPayment { MemberId = Member.Id, PaidOn = NewDuesPaid.Value });
+                }
+                if (NewOrientationAttended.HasValue)
+                {
+                    context.Add(new MembershipOrientation { MemberId = Member.Id, DateReceived = NewOrientationAttended.Value });
+                }
             }
-            if (NewOrientationAttended.HasValue)
+            finally
             {
-                context.Add(new MembershipOrientation { MemberId = Member.Id, DateReceived = NewOrientationAttended.Value });
                 await context.SaveChangesAsync();
+                navigation.NavigateTo("/members/");
             }
-            if (NewSuspensionStartDate.HasValue)
-            {
-                context.Add(new MembershipSuspension { MemberId = Member.Id, StartDate = NewSuspensionStartDate.Value, EndDate = NewSuspensionEndDate, Reason = NewSuspensionReason });
-                await context.SaveChangesAsync();
-            }
-            if (!string.IsNullOrEmpty(NewNote))
-            {
-                context.Add(new MembershipNote { MemberId = Member.Id, Note = NewNote });
-                await context.SaveChangesAsync();
-            }
-
-            navigation.NavigateTo("/members/");
         }
 
         private void MembershipTypeChange(ChangeEventArgs e)
@@ -87,13 +88,103 @@ namespace TNG.Web.Board.Pages.Membership
             Member.MemberType = membershipType;
         }
 
-        private static string SuspensionDisplay(MembershipSuspension? suspension)
+        private string SuspensionDisplay()
         {
-            if (suspension != null)
-                return suspension.EndDate.HasValue
-                ? $"Suspended until {suspension.EndDate.Value:MM/dd/yyy}"
+            if (ActiveSuspension != null)
+                return ActiveSuspension.EndDate.HasValue
+                ? $"Suspended until {ActiveSuspension.EndDate.Value:MM/dd/yyy}"
                 : "Blacklisted";
             return "In good standing";
+        }
+
+        private async void AddSuspension()
+        {
+            if (NewSuspensionStartDate.HasValue)
+            {
+                var note = (NewSuspensionEndDate.HasValue
+                        ? $"Member suspended from {NewSuspensionStartDate.Value:MM/dd/yyyy} until {NewSuspensionEndDate.Value:MM/dd/yyyy}"
+                        : $"Member is blacklisted starting {NewSuspensionStartDate.Value:MM/dd/yyyy}")
+                        + (!string.IsNullOrEmpty(NewSuspensionReason) ? $" Reason - {NewSuspensionReason}" : string.Empty);
+                await AddNote(note);
+
+                context.Add(new MembershipSuspension { MemberId = Member.Id, StartDate = NewSuspensionStartDate.Value, EndDate = NewSuspensionEndDate, Reason = NewSuspensionReason });
+                await context.SaveChangesAsync();
+
+                NewSuspensionStartDate = null;
+                NewSuspensionEndDate = null;
+                NewSuspensionReason = null;
+
+                AddSuspensionToggle = false;
+
+                StateHasChanged();
+            }
+        }
+
+        private async void DeleteSuspension()
+        {
+            await AddNote((ActiveSuspension!.EndDate.HasValue ? "Suspension" : "Blacklist") + " removed from member");
+
+            context.Remove(ActiveSuspension);
+
+            await context.SaveChangesAsync();
+
+            StateHasChanged();
+        }
+
+        private async void EndSuspension()
+        {
+            await AddNote((ActiveSuspension!.EndDate.HasValue ? "Suspension" : "Blacklist") + " ended early");
+
+            ActiveSuspension.EndDate = DateTime.Now;
+
+            await context.SaveChangesAsync();
+
+            StateHasChanged();
+        }
+
+        private async Task AddNote(string note)
+        {
+            if (!string.IsNullOrEmpty(note))
+            {
+                context.Add(new MembershipNote
+                {
+                    MemberId = Member.Id,
+                    Note = note
+                });
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private async void AddMemberNote()
+        {
+            if (!string.IsNullOrEmpty(NewNote))
+            {
+                await AddNote(NewNote);
+                NewNote = string.Empty;
+                StateHasChanged();
+            }
+        }
+
+        private async void DeleteDues()
+        {
+            if (Dues != null)
+            {
+                await AddNote($"Removed record of dues paid, previously {Dues.PaidOn:MM/dd/yyyy}");
+                context.Remove(Dues);
+                await context.SaveChangesAsync();
+                StateHasChanged();
+            }
+        }
+
+        private async void DeleteOrientation()
+        {
+            if (Orientation != null)
+            {
+                await AddNote($"Removed record of dues paid, previously {Orientation.DateReceived:MM/dd/yyyy}");
+                context.Remove(Orientation);
+                await context.SaveChangesAsync();
+                StateHasChanged();
+            }
         }
     }
 }
