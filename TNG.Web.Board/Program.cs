6ec7085a386f31c98467021334864ce893cd0306
Drake Perrior-Small <drakecsmall@gmail.com>
2026-02-16 06:19:06 -0700

    fix auth roles

--- TNG.Web.Board/Program.cs
+++ TNG.Web.Board/Program.cs
@@ -3,7 +3,6 @@ using Ixnas.AltchaNet;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
-using System.Net;
 using TNG.Web.Board.Data;
 using TNG.Web.Board.Services;
 using TNG.Web.Board.Utilities;
@@ -17,16 +16,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     ServiceLifetime.Transient,
     ServiceLifetime.Transient);
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddIdentityCore<IdentityUser>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = true;
-    })
-    .AddDefaultTokenProviders()
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddSignInManager()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders(); 
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
@@ -73,6 +67,8 @@ builder.Services.AddBlazoredModal();
 
 builder.Services.AddMemoryCache();
 
+builder.Services.AddCascadingAuthenticationState();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -91,19 +87,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
-app.Use(async (context, next) =>
-{
-    context.Response.Headers.Add("Content-Security-Policy", "default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; script-src-elem * data: blob: 'unsafe-inline' 'unsafe-eval';connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; media-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline';font-src * data: blob: 'unsafe-inline';frame-ancestors * data: blob:;");
-    await next();
-});
 
 //Square webhooks
 app.MapPost("/api/square/invoicepaid", SquareService.HandleInvoicePaid);
@@ -117,7 +106,6 @@ app.MapGet("/api/discord/byid/{discordId:long}", DiscordAPIService.GetMemberByDi
 app.MapGet("/api/discord/byid/info/{discordId:long}", DiscordAPIService.GetMemberInfoByDiscordId);
 app.MapGet("/api/discord/members", DiscordAPIService.GetMemberInfoForAllMembers);
 
-SecretCodeService.SetCode(builder.Configuration["TNGRegistrationCode"]);
 await RolesData.SeedRoles(app.Services);
 
 app.Run();
